466f56703af5fedb2b03f3858083e529e5dbb798
Pavel Kovalev <pkovalev.main@gmail.com>
2024-02-16 20:32:16 +0100

    lazyvim config

---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "noice.nvim", enabled = false },
+  { "dashboard-nvim", enabled = false },
+  { "friendly-snippets", enabled = false },
+  { "mini.pairs", enabled = false },
+  { "mini.surround", enabled = false },
+  { "mini.ai", enabled = false },
+  { "LuaSnip", enabled = false },
+}
