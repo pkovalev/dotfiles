475799a3b16aaa06cc013f2904037b459ac1dd78
Pavel Kovalev <82114114+pkovalev-alianza@users.noreply.github.com>
2024-06-03 11:24:02 +0200

    yank highlight

--- configure/nvim/init.lua
+++ configure/nvim/init.lua
@@ -30,3 +30,10 @@ spec = {
 
 vim.cmd.colorscheme "catppuccin-mocha"
 
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", {clear = true}),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
