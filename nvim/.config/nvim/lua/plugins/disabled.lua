ac48c67238548ea8d90e8149c5198abafa3e0063
pkovalev <pkovalev@debian-vm>
2025-01-01 12:24:48 -0500

    Simplify file structure

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "noice.nvim", enabled = false },
-  { "dashboard-nvim", enabled = false },
-  { "friendly-snippets", enabled = false },
-  { "mini.pairs", enabled = false },
-  { "mini.surround", enabled = false },
-  { "mini.ai", enabled = false },
-  { "LuaSnip", enabled = false },
-}
